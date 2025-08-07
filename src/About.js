3cda67a9a477148a80e2c3e96d50288a907269f4
you Name <you@example.com>
2025-08-07 19:54:21 +0530

    adding about page

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import { useState } from 'react';
+import './header.css';
+
+function About() {
+   
+  
+  return (
+    
+   <div className="header">
+
+    
+        <div className='headings' >
+            <button className='header-style'>Home</button>
+            <button className='header-style'>about</button>
+            <button className='header-style'>services</button>
+            <button className='header-style'>contact</button>
+        </div>
+   </div>
+  );
+}
+
+export default About;
